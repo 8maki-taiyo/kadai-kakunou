a01fad3981616f565babc0ad3c0d0c1675413955
8maki-taiyo <138664999+8maki-taiyo@users.noreply.github.com>
2024-07-24 08:26:29 +0900

    Add files via upload

--- kadai/abc362/abc362_c_八巻.py
+++ kadai/abc362/abc362_c_八巻.py
@@ -4,20 +4,23 @@ def main():
     R = [0]*n
     for i in range(n):
         L[i], R[i] = map(int, input().split())
-    T = 0
     X = [L[j] for j in range(n)]
+    # sは0までの距離
     s = sum(X)
 
+    # 最小の組み合わせの合計の時点で0を超えていたら"NO"を出す
     if s > 0:
         print("No")
         exit()
 
     for k in range(n):
-        d = min(T-s, R[k]-L[k])
+        # LとRの差、もしくは0になるまでに必要な数値をlistに入れる
+        d = min(-s, R[k]-L[k])
         X[k] += d
+        # list に追加した分だけ0までの距離が縮まる
         s += d
 
-    if s == T:
+    if s == 0:
         print("Yes")
         print(*X)
     else:
